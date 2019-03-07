b9dbba6951f2b9ba32afafd7ebe22d845a718e7f
AbelW <unoctium1@gmail.com>
2019-03-07 13:23:31 -0800

    Created project

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 initRotation;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(initRotation * Time.deltaTime);
+    }
+}
